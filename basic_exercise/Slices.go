c0edf6516b2c6c774abfdaad9a1e3a787993b90a
zhangslob <zhangslob@gmail.com>
2018-08-16 15:33:25 +0800

    add some exer

---
+++ basic_exercise/Slices.go
@@ -0,0 +1,39 @@
+//Slices are a key data type in Go, giving a more powerful interface to sequences than arrays.
+
+package main
+
+import "fmt"
+
+func main() {
+	s := make([]string, 3)
+	fmt.Println("emp: ", s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set: ", s)
+	fmt.Println("get: ", s[2])
+
+	fmt.Println("len: ", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd: ", s)
+
+	//Slices can also be copy’d. Here we create an empty slice c of the same length as s and copy into c from s.
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy: ", c, s)
+
+	l := s[2:5]
+	fmt.Println("sl1: ", l)
+
+	l = s[:5]
+	fmt.Println("sl2: ", l)
+
+	l = s[2:]
+	fmt.Println("sl3: ", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl: ", t)
+}
